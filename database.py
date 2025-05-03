67402386db0a6bc4c8f58c30102f704fb30012f1
Louis Foulou <louis.foulou@telecomnancy.eu>
2025-05-03 20:35:35 +0200

    Added code to load deck metadata into the db.

--- database.py
+++ database.py
@@ -1,10 +1,15 @@
 import sqlite3
 from flask import g
-from os import path
+from os import path, walk
+from pathlib import Path
+from json import load, JSONDecodeError
+from sys import exit
+
 
 
 # Database name.
 DATABASE = 'app.db'
+
 
 
 # Opens the database.
@@ -19,12 +24,66 @@ def get_db():
 
 # Closes the database.
 def close_db(e=None):
-    db = g.pop('db', None)
-    if db is not None:
-        db.close()
+	db = g.pop('db', None)
+	if db is not None:
+		db.close()
+
+
+
+# Adds a deck in the database and returns it's id.
+def create_deck(db, meta, json_file):
+
+	# Inserts the deck.
+	cursor = db.execute(f"INSERT INTO Deck (deck_name) VALUES ('{meta["name"]}');")
+	deck_id = cursor.lastrowid
+	
+	# Inserts the tags.
+	tags = meta.get("tags", [])
+	if isinstance(tags, list) and all(isinstance(tag, str) for tag in tags):
+		if tags:
+			placeholders = ", ".join(["(?, ?)"] * len(tags))
+			values = []
+			for tag in tags:
+				values.extend([deck_id, tag])
+			db.execute(
+				f"INSERT INTO DeckTag (deck_id, tag) VALUES {placeholders}",
+				values
+			)
+	else:
+		print(f"[SESHAT]: error: deck '{json_file}' tags must be a list of strings")
+		exit(1)
+	return deck_id
+
+
 
 # Initialises the database.
 def init_db():
+	print(f"[SESHAT]: info: database initialization")
+
+	# Creates the database.
 	db = get_db()
-	with open('schema.sql') as f:
+	with open("schema.sql") as f:
 		db.executescript(f.read())
+
+	# Gets all the json deck files.
+	json_dir = Path("static/data/new-deck/")
+	for json_file in json_dir.glob("**/*.json"):
+		with open(json_file, "r", encoding="utf-8") as f:
+			try:
+
+				# Loads the json data.
+				data = load(f)
+				meta = data["meta"]
+				if not isinstance(meta, dict) or not isinstance(meta.get("name"), str):
+					print(f"[SESHAT]: error: deck '{json_file}' meta is invalid")
+					exit(1)
+
+				# Adds the data to the database.
+				create_deck(db, meta, json_file)
+				print(f"[SESHAT]: info: {json_file} loaded into the database")
+
+			except JSONDecodeError as e:
+				print(f"[SESHAT]: error: deck '{json_file}' couldn't be loaded : {e}")
+				exit(1)
+	db.commit()
+	print(f"[SESHAT]: info: database initialized")
--- schema.sql
+++ schema.sql
@@ -1,5 +1,99 @@
-CREATE TABLE user (
+-- Basic Flashcard Element.
+CREATE TABLE Element (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    email TEXT NOT NULL UNIQUE
+    deck_id INTEGER NOT NULL,
+    element_type TEXT NOT NULL CHECK (element_type IN ('radical', 'kanji', 'word')),
+    japanese_name TEXT NOT NULL, 
+    translation TEXT NOT NULL,
+    mnemonic TEXT,
+    FOREIGN KEY (deck_id) REFERENCES Deck(id) ON DELETE CASCADE
+);
+
+-- Meaning of an Element.
+-- Element  1 <--> n  Meaning
+CREATE TABLE Meaning (
+    element_id INTEGER NOT NULL,
+    meaning TEXT NOT NULL,
+    PRIMARY KEY (element_id, meaning),
+    FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
+);
+
+-- Example of use of an Element.
+-- Element  1 <--> n  Example
+CREATE TABLE Example (
+    element_id INTEGER NOT NULL,
+    example TEXT NOT NULL,
+    PRIMARY KEY (element_id, example),
+    FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
+);
+
+
+
+-- The reading of a kanji. Can be kun'yomi or on'yomi.
+-- Element  1 <--> n  KanjiReading
+CREATE TABLE KanjiReading (
+    element_id INTEGER NOT NULL,
+    reading TEXT NOT NULL,
+    reading_type TEXT NOT NULL CHECK (reading_type IN ('on', 'kun')),
+    PRIMARY KEY (element_id, reading),
+    FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
+);
+
+-- Radicals composing a kanji.
+-- Element  1 <--> n  KanjiRadical
+CREATE TABLE KanjiRadical (
+    element_id INTEGER NOT NULL,
+    radical TEXT NOT NULL,
+    PRIMARY KEY (element_id, radical),
+    FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
+);
+
+
+
+-- Informations about a word
+-- Element  1 <--> 1  WordInfo
+CREATE TABLE WordInfo (
+    element_id INTEGER PRIMARY KEY,
+    reading TEXT NOT NULL,
+    word_class TEXT NOT NULL,
+    FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
+);
+
+
+
+-- The reading of a radical.
+-- Element  1 <--> 1  RadicalReading
+CREATE TABLE RadicalReading (
+    element_id INTEGER PRIMARY KEY,
+    reading TEXT NOT NULL,
+    FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
+);
+
+
+
+-- This contains the score of an element.
+-- Element  1 <--> 1  Score
+CREATE TABLE Score (
+    element_id INTEGER PRIMARY KEY,
+    last_review DATE,
+    validation_count INTEGER NOT NULL DEFAULT 0,
+    difficulty REAL NOT NULL DEFAULT 1.0,
+    FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
+);
+
+
+
+-- This contains all the decks.
+-- Element  1 <--> 0  Deck
+CREATE TABLE Deck (
+    deck_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    deck_name TEXT NOT NULL
+);
+
+-- This contains all the deck tags.
+-- Deck  1 <--> n  DeckTag
+CREATE TABLE DeckTag (
+    deck_id INTEGER PRIMARY KEY,
+    tag TEXT NOT NULL,
+    FOREIGN KEY (deck_id) REFERENCES Deck(id) ON DELETE CASCADE
 );
