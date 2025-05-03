b3271ac741db06ec8835dafeb21ae2c93529705b
Louis Foulou <louis.foulou@telecomnancy.eu>
2025-05-03 23:37:56 +0200

    Added a loading system for the database creation. Currently, only the radical decks are being loaded.

--- database.py
+++ database.py
@@ -30,6 +30,30 @@ def close_db(e=None):
 
 
 
+
+# Constants to check the structure of jsons.
+_radical_fields = ["front", "name", "meaning", "mnemonic", "example"]
+_radical_field_islist = [False, False, True, False, True]
+
+# Checks the structure of a card dict.
+def check_card_structure(json, fields, lists):
+	if not isinstance(json, dict): return False
+	for i in range(len(fields)):
+		t = list if _radical_field_islist[i] else str
+		e = json.get(_radical_fields[i], None)
+		if not isinstance(e, t): return False
+		if isinstance(e, list):
+			for elt in e :
+				if not isinstance(elt, str): return False
+	return True
+
+# Returns the next id that will be used as primary key by an autoincrement.
+def get_next_autoincrement_id(db, table):
+	result = db.execute(f"SELECT seq FROM sqlite_sequence WHERE name='{table}'").fetchone()
+	if result: return result[0] + 1
+	else: return 1
+
+
 # Adds a deck in the database and returns it's id.
 def create_deck(db, meta, json_file):
 
@@ -54,6 +78,75 @@ def create_deck(db, meta, json_file):
 		exit(1)
 	return deck_id
 
+# Adds all the cards from a deck to the database.
+def create_cards(db, deck_id, cards, tags, json_file):
+	r = "radical" in tags
+	w = "word" in tags
+	k = "kanji" in tags
+	if r and not w and not k:
+		create_cards_radical(db, deck_id, cards, json_file)
+	elif not r and w and not k:
+		create_cards_word(db, deck_id, cards, json_file)
+	elif not r and not w and k:
+		create_cards_kanji(db, deck_id, cards, json_file)
+	else :
+		print(f"[SESHAT]: error: deck '{json_file}' tags must contains radical, word or kanji")
+		exit(1)
+
+# Adds all the radical cards from a deck to the database.
+def create_cards_radical(db, deck_id, cards, json_file):
+	next_id = get_next_autoincrement_id(db, "Element")
+
+	# Gets all the values from the cards.
+	cards_values = []
+	meaning_values = []
+	example_values = []
+	reading_values = []
+	for i in range(len(cards)):
+		card = cards[i]
+		if not check_card_structure(card, _radical_fields, _radical_field_islist):
+			print(f"[SESHAT]: error: deck '{json_file}' card {i+1} is invalid")
+			exit(1)
+
+		# Stores the values in lists.
+		cards_values.extend([deck_id, "radical", card["front"], card["mnemonic"]])
+		for meaning in card["meaning"]:
+			meaning_values.extend([next_id, meaning])
+		for example in card["example"]:	
+			example_values.extend([next_id, example])
+		reading_values.extend([next_id, card["name"]])
+		next_id += 1
+
+	# Executes the requests to insert the values.
+	cards_placeholder = ", ".join(["(?,?,?,?)"] * int(len(cards_values)/4))
+	meaning_placeholder = ", ".join(["(?,?)"] * int(len(meaning_values)/2))
+	example_placeholder = ", ".join(["(?,?)"] * int(len(example_values)/2))
+	reading_placeholder = ", ".join(["(?,?)"] * int(len(reading_values)/2))
+	db.execute(
+		f"INSERT INTO Element (deck_id, element_type, japanese_name, mnemonic) VALUES {cards_placeholder}",
+		cards_values
+	)
+	db.execute(
+		f"INSERT INTO Meaning (element_id, meaning) VALUES {meaning_placeholder}",
+		meaning_values
+	)
+	db.execute(
+		f"INSERT INTO Example (element_id, example) VALUES {example_placeholder}",
+		example_values
+	)
+	db.execute(
+		f"INSERT INTO RadicalReading (element_id, reading) VALUES {reading_placeholder}",
+		reading_values
+	)
+
+# Adds all the word cards from a deck to the database.
+def create_cards_word(db, deck_id, cards, json_file):
+	pass
+
+# Adds all the kanji cards from a deck to the database.
+def create_cards_kanji(db, deck_id, cards, json_file):
+	pass
+
 
 
 # Initialises the database.
@@ -74,16 +167,21 @@ def init_db():
 				# Loads the json data.
 				data = load(f)
 				meta = data["meta"]
-				if not isinstance(meta, dict) or not isinstance(meta.get("name"), str):
+				cards = data["cards"]
+				if not isinstance(meta, dict) or not isinstance(meta.get("name"), str) \
+				or not isinstance(cards, list) or not isinstance(meta.get("tags"), list):
 					print(f"[SESHAT]: error: deck '{json_file}' meta is invalid")
 					exit(1)
+				tags = meta["tags"]
 
 				# Adds the data to the database.
-				create_deck(db, meta, json_file)
+				deck_id = create_deck(db, meta, json_file)
+				create_cards(db, deck_id, cards, tags, json_file)
+				
 				print(f"[SESHAT]: info: {json_file} loaded into the database")
 
 			except JSONDecodeError as e:
 				print(f"[SESHAT]: error: deck '{json_file}' couldn't be loaded : {e}")
 				exit(1)
 	db.commit()
-	print(f"[SESHAT]: info: database initialized")
+	print(f"[SESHAT]: info: database initialized\n")
--- schema.sql
+++ schema.sql
@@ -1,30 +1,29 @@
 -- Basic Flashcard Element.
 CREATE TABLE Element (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    deck_id INTEGER NOT NULL,
-    element_type TEXT NOT NULL CHECK (element_type IN ('radical', 'kanji', 'word')),
-    japanese_name TEXT NOT NULL, 
-    translation TEXT NOT NULL,
-    mnemonic TEXT,
-    FOREIGN KEY (deck_id) REFERENCES Deck(id) ON DELETE CASCADE
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	deck_id INTEGER NOT NULL,
+	element_type TEXT NOT NULL CHECK (element_type IN ('radical', 'kanji', 'word')),
+	japanese_name TEXT NOT NULL, 
+	mnemonic TEXT NOT NULL,
+	FOREIGN KEY (deck_id) REFERENCES Deck(id) ON DELETE CASCADE
 );
 
 -- Meaning of an Element.
 -- Element  1 <--> n  Meaning
 CREATE TABLE Meaning (
-    element_id INTEGER NOT NULL,
-    meaning TEXT NOT NULL,
-    PRIMARY KEY (element_id, meaning),
-    FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
+	element_id INTEGER NOT NULL,
+	meaning TEXT NOT NULL,
+	PRIMARY KEY (element_id, meaning),
+	FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
 );
 
 -- Example of use of an Element.
 -- Element  1 <--> n  Example
 CREATE TABLE Example (
-    element_id INTEGER NOT NULL,
-    example TEXT NOT NULL,
-    PRIMARY KEY (element_id, example),
-    FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
+	element_id INTEGER NOT NULL,
+	example TEXT NOT NULL,
+	PRIMARY KEY (element_id, example),
+	FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
 );
 
 
@@ -32,20 +31,20 @@ CREATE TABLE Example (
 -- The reading of a kanji. Can be kun'yomi or on'yomi.
 -- Element  1 <--> n  KanjiReading
 CREATE TABLE KanjiReading (
-    element_id INTEGER NOT NULL,
-    reading TEXT NOT NULL,
-    reading_type TEXT NOT NULL CHECK (reading_type IN ('on', 'kun')),
-    PRIMARY KEY (element_id, reading),
-    FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
+	element_id INTEGER NOT NULL,
+	reading TEXT NOT NULL,
+	reading_type TEXT NOT NULL CHECK (reading_type IN ('on', 'kun')),
+	PRIMARY KEY (element_id, reading),
+	FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
 );
 
 -- Radicals composing a kanji.
 -- Element  1 <--> n  KanjiRadical
 CREATE TABLE KanjiRadical (
-    element_id INTEGER NOT NULL,
-    radical TEXT NOT NULL,
-    PRIMARY KEY (element_id, radical),
-    FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
+	element_id INTEGER NOT NULL,
+	radical TEXT NOT NULL,
+	PRIMARY KEY (element_id, radical),
+	FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
 );
 
 
@@ -53,10 +52,10 @@ CREATE TABLE KanjiRadical (
 -- Informations about a word
 -- Element  1 <--> 1  WordInfo
 CREATE TABLE WordInfo (
-    element_id INTEGER PRIMARY KEY,
-    reading TEXT NOT NULL,
-    word_class TEXT NOT NULL,
-    FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
+	element_id INTEGER PRIMARY KEY,
+	reading TEXT NOT NULL,
+	word_class TEXT NOT NULL,
+	FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
 );
 
 
@@ -64,9 +63,9 @@ CREATE TABLE WordInfo (
 -- The reading of a radical.
 -- Element  1 <--> 1  RadicalReading
 CREATE TABLE RadicalReading (
-    element_id INTEGER PRIMARY KEY,
-    reading TEXT NOT NULL,
-    FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
+	element_id INTEGER PRIMARY KEY,
+	reading TEXT NOT NULL,
+	FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
 );
 
 
@@ -74,11 +73,11 @@ CREATE TABLE RadicalReading (
 -- This contains the score of an element.
 -- Element  1 <--> 1  Score
 CREATE TABLE Score (
-    element_id INTEGER PRIMARY KEY,
-    last_review DATE,
-    validation_count INTEGER NOT NULL DEFAULT 0,
-    difficulty REAL NOT NULL DEFAULT 1.0,
-    FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
+	element_id INTEGER PRIMARY KEY,
+	last_review DATE,
+	validation_count INTEGER NOT NULL DEFAULT 0,
+	difficulty REAL NOT NULL DEFAULT 1.0,
+	FOREIGN KEY (element_id) REFERENCES Element(id) ON DELETE CASCADE
 );
 
 
@@ -86,14 +85,14 @@ CREATE TABLE Score (
 -- This contains all the decks.
 -- Element  1 <--> 0  Deck
 CREATE TABLE Deck (
-    deck_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    deck_name TEXT NOT NULL
+	deck_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	deck_name TEXT NOT NULL
 );
 
 -- This contains all the deck tags.
 -- Deck  1 <--> n  DeckTag
 CREATE TABLE DeckTag (
-    deck_id INTEGER PRIMARY KEY,
-    tag TEXT NOT NULL,
-    FOREIGN KEY (deck_id) REFERENCES Deck(id) ON DELETE CASCADE
+	deck_id INTEGER PRIMARY KEY,
+	tag TEXT NOT NULL,
+	FOREIGN KEY (deck_id) REFERENCES Deck(id) ON DELETE CASCADE
 );
